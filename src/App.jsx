c103f1a23b1dc1c6feded944568c7efdfbe9d70c
MathiasHun1 <141583942+MathiasHun1@users.noreply.github.com>
2024-06-28 16:39:53 +0200

    added fetching openings data

--- src/App.jsx
+++ src/App.jsx
@@ -5,61 +5,29 @@ import {
 } from 'react-router-dom'
 import loading2 from './assets/loading2.gif'
 import Header from "./components/Header"
-import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 import Aside from "./components/Aside"
 import services from './services/data'
-import ReviewCard from "./components/ReviewCard"
 
 
 function App() {
-  const [isDataLoaded, setIsDataLoaded] = useState(true)
-  const [openingsData, setData] = useState(services.openings)
-  const [reviewsList, setReviewsList] = useState(null)
-  const [reviewsQuant , setReviewsQuant] = useState(null)
-  const [reviewsAvg, setReviewsAvg] = useState(null)
+  const [isDataLoaded, setIsDataLoaded] = useState(false)
+  const [openingsData, setOpeningsData] = useState(null)
 
-  // useEffect(() => {
-  //   services.getOpenings()
-  //     .then(intitialOpenings => setData(intitialOpenings))
-  //   setData(services.openings)
-  //   services.getReviewsList()
-  //     .then(response => {
-  //       setReviewsList(response.reviews) 
-  //     })
-  //   services.getRatingAvg()
-  //     .then(response => setReviewsAvg(response.rating))
-  //   services.getRatingsQuant()
-  //     .then(response => setReviewsQuant(response.user_ratings_total))
-  // }, [])
+  useEffect(() => {
+    services.getAllDays()
+      .then(initialOpenings => setOpeningsData(initialOpenings)) 
+  }, [])
 
-
-  // useEffect(() => {
-  //   if(openingsData && reviewsList && reviewsQuant && reviewsAvg) {
-  //     setIsDataLoaded(true)
-  //   }
-  // }, [openingsData, reviewsList, reviewsQuant, reviewsAvg])
+  useEffect(() => {
+    console.log(openingsData)
+  }, [openingsData])
 
   if(isDataLoaded) {
     return (
         <div className="relative">
           <Router>
               <Header openingsData={openingsData} />
-              {/* <div className="w-full h-36 grid grid-cols-4 opacity-55">
-                <div>
-                  <img src={bmw1} alt="" />
-                </div>
-                <div>
-                  <img src={cruiser1} alt="" /> 
-                </div>
-                <div>
-                  <img src={bmw3} alt="" />
-                </div>
-                <div>
-                  <img src={bmw4} alt="" />
-                </div>
-              </div> */}
-              {/* <Navbar /> */}
               <div className="flex flex-row">
                 <Main openingsData={openingsData} />
                 <Aside />
--- src/services/data.js
+++ src/services/data.js
@@ -1,91 +1,10 @@
 import axios from 'axios'
-import { format, parse, isAfter, isBefore, getDay } from 'date-fns'
 
-// const getOpenings = () => {
-//   const request = axios
-//   .get(`api/maps/api/place/details/json?place_id=${import.meta.env.VITE_GOOGLE_PLACE_ID}&key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&fields=name,opening_hours`)
-//   return request.then(response => response.data.result.opening_hours)
-// }
+const baseUrl = 'https://tata-backend-7rys.onrender.com/api/openings'
 
-// const getReviewsList = () => {
-//   const request = axios
-//     .get(`api/maps/api/place/details/json?place_id=${import.meta.env.VITE_GOOGLE_PLACE_ID}&key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&fields=reviews&language=hu`)
-//     return request.then(response => response.data.result)
-// }
-
-// const getRatingAvg = () => {
-//   const request = axios
-//     .get(`api/maps/api/place/details/json?place_id=${import.meta.env.VITE_GOOGLE_PLACE_ID}&key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&fields=rating`)
-//     return request.then(response => response.data.result)
-// }
-
-// const getRatingsQuant = () => {
-//   const request = axios
-//     .get(`api/maps/api/place/details/json?place_id=${import.meta.env.VITE_GOOGLE_PLACE_ID}&key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&fields=user_ratings_total`)
-//     return request.then(response => response.data.result)
-// }
-
-
-
-
-const isOpenNow = () => {
-  const openingTimes = {
-    monday: {
-      opening: '09:00',
-      closing: '17:00'
-    },
-    tuesday: {
-      opening: '09:00',
-      closing: '17:00'
-    },
-    wednesday: {
-      opening: '10:00',
-      closing: '17:00'
-    },
-    thursday: {
-      opening: '09:00',
-      closing: '17:00'
-    },
-    friday: {
-      opening: '09:00',
-      closing: '17:00'
-    }
-  };
-
-  const now = new Date();
-  const dayOfWeek = getDay(now); // 0 (Sunday) to 6 (Saturday)
-  const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
-
-  // Check if it's a weekend
-  if (dayOfWeek === 0 || dayOfWeek === 6) {
-    return false;
-  }
-
-  const dayName = days[dayOfWeek];
-  const dayTimes = openingTimes[dayName];
-
-  if (!dayTimes) {
-    return false; // No opening times for this day
-  }
-
-  const openingDate = parse(dayTimes.opening, 'HH:mm', now);
-  const closingDate = parse(dayTimes.closing, 'HH:mm', now);
-
-  return isAfter(now, openingDate) && isBefore(now, closingDate);
+const getAllDays = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response =>  response.data)
 }
 
-const openings = {
-  open_now: isOpenNow(),
-  weekday_text: [
-    "hétfő: 9:00–17:00",
-    "kedd: 9:00–17:00",
-    "szerda: 10:00–17:00",
-    "csütörtök: 9:00–17:00",
-    "péntek: 9:00–17:00",
-    "szombat: Zárva",
-    "vasárnap: Zárva"
-  ]
-}
-
-
-export default { openings }
+export default { getAllDays }
